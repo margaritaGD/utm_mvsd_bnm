54222f3ea63edda9b5e545a75205cf9321d9552b
mmarfoi <mmarfoi@griddynamics.com>
2024-10-10 19:05:16 +0300

    it works a little

--- src/web/chatbot.py
+++ src/web/chatbot.py
@@ -1,12 +1,18 @@
+import os
+
 from openai import OpenAI
+from dotenv import load_dotenv
 import streamlit as st
 
-with st.sidebar:
-    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
-    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+load_dotenv()
+openai_api_key = os.getenv("OPENAIKEY")
 
-st.title("💬 Chatbot")
-st.caption("🚀 A Streamlit chatbot powered by OpenAI")
+with st.sidebar:
+    "[View the source code](https://github.com/margaritaGD/utm_mvsd_bnm)"
+    "[![{tred_id}](tred_id)]"
+
+st.title("BNM codex Chatbot")
+
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
@@ -14,7 +20,6 @@ for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
 if prompt := st.chat_input():
-
     client = OpenAI(api_key=openai_api_key)
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
